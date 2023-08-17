bc8555a764b32579e286dfd0b08115cec65abce3
Vasu <vasumendapara93@gmail.com>
2023-08-17 22:00:04 +0530

    Revert "populate navigation property"
    This reverts commit df08bde3767eefce3211e4ddf6254e703cef2662.

--- Ecommerce.DataAccess/Repository/IRepository/IRepository.cs
+++ Ecommerce.DataAccess/Repository/IRepository/IRepository.cs
@@ -9,8 +9,8 @@ namespace Ecommerce.DataAccess.Repository.IRepository
 {
     public interface IRepository<T> where T : class
     {
-        IEnumerable<T> GetAll(string? includeProperties = null);
-        T Get(Expression<Func<T,bool>>filter, string? includeProperties = null);
+        IEnumerable<T> GetAll();
+        T Get(Expression<Func<T,bool>>filter);
         void Add(T entity);
         void Remove(T entity);
         void RemoveRange(IEnumerable<T> entity);
--- Ecommerce.DataAccess/Repository/ProductRepository.cs
+++ Ecommerce.DataAccess/Repository/ProductRepository.cs
@@ -19,23 +19,7 @@ namespace Ecommerce.DataAccess.Repository
 
         public void Update(Product obj)
         {
-            var objFromDb = _db.Products.FirstOrDefault(u => u.Id == obj.Id);
-            if(objFromDb != null)
-            {
-                objFromDb.Title = obj.Title;
-                objFromDb.Description = obj.Description;
-                objFromDb.CategoryId = obj.CategoryId;
-                objFromDb.ISBN = obj.ISBN;
-                objFromDb.Price = obj.Price;
-                objFromDb.Price50 = obj.Price50;
-                objFromDb.Price100 = obj.Price100;
-                objFromDb.ListPrice = obj.ListPrice;
-                objFromDb.Author = obj.Author;
-                if(obj.ImageUrl != null)
-                {
-                    objFromDb.ImageUrl = obj.ImageUrl;
-                }
-            }
+            _db.Products.Update(obj);
         }
     }
 }
