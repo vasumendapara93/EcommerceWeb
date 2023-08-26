355702dd791f2e09cf1d01daeea029784689e953
Vasu <vasumendapara93@gmail.com>
2023-08-26 15:44:24 +0530

    Extended Identity User

--- Ecommerce.DataAccess/Data/ApplicationDbContext.cs
+++ Ecommerce.DataAccess/Data/ApplicationDbContext.cs
@@ -14,6 +14,7 @@ namespace Ecommerce.DataAccess
 
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
