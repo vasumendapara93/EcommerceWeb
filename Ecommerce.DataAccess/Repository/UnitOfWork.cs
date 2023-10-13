2e85ec497747123d1e3754a5d92f67d927ebd45c
Vasu <vasumendapara93@gmail.com>
2023-10-13 17:11:37 +0530

    added into unitofwork and iunitofwork

--- Ecommerce.DataAccess/Repository/UnitOfWork.cs
+++ Ecommerce.DataAccess/Repository/UnitOfWork.cs
@@ -15,7 +15,8 @@ namespace Ecommerce.DataAccess.Repository
         public ICompanyRepository Company { get; private set; }
         public IProductRepository Product { get; private set; }
         public IShoppingCartRepository ShoppingCart { get; private set; }
-        
+        public IOrderDetailRepository OrderDetail { get; private set; }
+        public IOrderHeaderRepository OrderHeader { get; private set; }
         public UnitOfWork(ApplicationDbContext db) 
         { 
             _db = db;
@@ -24,6 +25,8 @@ namespace Ecommerce.DataAccess.Repository
             Product = new ProductRepository(_db);
             Company = new CompanyRepository(_db);
             ShoppingCart = new ShoppingCartRepository(_db);
+            OrderDetail = new OrderDetailRepository(_db);
+            OrderHeader = new OrderHeaderRepository(_db);
         }
         public void Save()
         {
