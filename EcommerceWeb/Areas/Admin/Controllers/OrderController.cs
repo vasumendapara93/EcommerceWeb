633db6af0c26ead5b55d99d850fdf6193093db5b
Vasu <vasumendapara93@gmail.com>
2023-10-22 11:19:37 +0530

    Only Admin and Employee can see all orders

--- EcommerceWeb/Areas/Admin/Controllers/OrderController.cs
+++ EcommerceWeb/Areas/Admin/Controllers/OrderController.cs
@@ -5,6 +5,7 @@ using Ecommerce.Utility;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.Security.Claims;
 
 namespace EcommerceWeb.Areas.Admin.Controllers
 {
@@ -65,10 +66,24 @@ namespace EcommerceWeb.Areas.Admin.Controllers
         [HttpGet]
             public IActionResult GetAll(string status)
             {
-                IEnumerable<OrderHeader> objOrderHeaders = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList();
+            IEnumerable<OrderHeader> objOrderHeaders;
 
 
-                switch (status)
+            if (User.IsInRole(SD.Role_Admin) || User.IsInRole(SD.Role_Employee))
+            {
+                objOrderHeaders = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList();
+            }
+            else
+            {
+
+                var claimsIdentity = (ClaimsIdentity)User.Identity;
+                var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
+
+                objOrderHeaders = _unitOfWork.OrderHeader.GetAll(u => u.ApplicationUserId == userId, includeProperties: "ApplicationUser");
+            }
+
+
+            switch (status)
                 {
                     case "pending":
                         objOrderHeaders = objOrderHeaders.Where(u => u.PaymentStatus == SD.PaymentStatusDelayedPayment);
