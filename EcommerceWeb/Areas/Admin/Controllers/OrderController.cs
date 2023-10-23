eacc9b17082afc4188426a9e3d820a6823e6aa7a
Vasu <vasumendapara93@gmail.com>
2023-10-23 14:34:52 +0530

    Created MVC for Order Manage

---
+++ EcommerceWeb/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,35 @@
+﻿using Ecommerce.DataAccess.Repository.IRepository;
+using Ecommerce.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EcommerceWeb.Areas.Admin.Controllers
+{
+    [Area("admin")]
+    public class OrderController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public OrderController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        #region API CALLS
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            List<OrderHeader> objOrderHeaders = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList();
+            return Json(new { data = objOrderHeaders });
+        }
+
+
+        #endregion
+    }
+}
