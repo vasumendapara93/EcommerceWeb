ed2403f5bce640bd11bfa5b9d1b1f2be669a9719
Vasu <vasumendapara93@gmail.com>
2023-10-26 21:06:09 +0530

    UI color changed

--- EcommerceWeb/wwwroot/js/order.js
+++ EcommerceWeb/wwwroot/js/order.js
@@ -39,7 +39,7 @@ function loadDataTable(status) {
                 data: 'id',
                 "render": function (data) {
                     return `<div class="w-75 btn-group" role="group">
-                     <a href="/admin/order/details?orderId=${data}" class="btn btn-primary mx-2"> <i class="bi bi-pencil-square"></i></a>               
+                     <a href="/admin/order/details?orderId=${data}" class="btn btn-info mx-2"> <i class="bi bi-pencil-square"></i></a>               
                     
                     </div>`
                 },
--- EcommerceWeb/wwwroot/js/product.js
+++ EcommerceWeb/wwwroot/js/product.js
@@ -17,8 +17,8 @@ function loadDataTable() {
                 data: 'id',
                 "render": function (data) {
                     return `<div class="w-75 btn-group" role="group">
-                     <a href="/admin/product/upsert?id=${data}" class="btn btn-primary mx-2"> <i class="bi bi-pencil-square"></i> Edit</a>               
-                     <a onclick=Delete('/admin/product/delete/${data}') class="btn btn-danger mx-2"> <i class="bi bi-trash-fill"></i> Delete</a>
+                     <a href="/admin/product/upsert?id=${data}" class="btn btn-info mx-2"> <i class="bi bi-pencil-square"></i> Edit</a>               
+                     <a onclick=Delete('/admin/product/delete/${data}') class="btn btn-warning mx-2"> <i class="bi bi-trash-fill"></i> Delete</a>
                     </div>`
                 },
                 "width": "25%"
